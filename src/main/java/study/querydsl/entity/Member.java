c1c3f82fd441c412bf984adb99cca1c3e0c73ed3
Ran <lan4250@naver.com>
2023-04-09 17:35:01 +0900

    removed cached

---
+++ src/main/java/study/querydsl/entity/Member.java
@@ -0,0 +1,64 @@
+package study.querydsl.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
+public class Member {
+
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username;
+
+    private int age;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+
+        }
+        this.team = team;
+    }
+
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+
+    }
+
+
+}
---
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,17 @@
+package study.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import study.querydsl.entity.QHello;
+
+@SpringBootTest
+class QuerydslApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+//		QHello qHello = new QHello();
+
+	}
+
+}
